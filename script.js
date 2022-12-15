cc2e52fb3e41f2bf7e93a7f55b1033a4543ea148
horuji <alexivanson392@gmail.com>
2022-12-15 15:41:57 +0300

    a

---
+++ script.js
@@ -0,0 +1,7 @@
+let minM = document.querySelector(".mini_manu");
+let M = document.querySelector(".menu");
+let an = document.querySelector(".manu_anim") 
+
+minM.addEventListener ("click", ()=>{ 
+    M.classList.toggle('menu')
+    M.classList.toggle('menu_show')} )
